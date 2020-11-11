5114baba235c58d37b72b727dc0d82c329391e94
Charlie Gador <charlie.gador@skola.botkyrka.se>
2020-11-11 14:09:14 +0100

    Task 3

--- 4/script.js
+++ 4/script.js
@@ -1,9 +1,13 @@
 // 1. Skapa en variabel som innehåller en array med fyra strängar, t.ex. fyra namn på personer.
-
+const arr = ["Hej ", "Hej igen ", "Tjaa ", " Hejdå"]
 // 2. Logga ut arrayen.
+console.log(arr)
 
 // 3. Byt ut det andra elementet till något annat.
+arr[1] = "Fanta"
 
 // 4. Logga ut arrayen igen.
+console.log(arr)
 
 // 5. Logga ut det sista elementet i arrayen.
+console.log(arr[3])
